00bfeb2749d850ea7384aadf1fd54abe1cb841c1
Rafael Vicente Santrovitsch <rafael.santrovitsch@estudante.ufjf.br>
2023-12-10 19:06:54 -0300

    Mudança visando corrigir erros

--- main.js
+++ main.js
@@ -9,6 +9,137 @@ let pontosOponente = 0;
 
 let jogadorAtual = "jogador";
 
+function iniciarRodadaJogador() {
+    alert('Sua vez, role um dado');
+    document.getElementById('rodarDado').addEventListener('click', rolarDado);
+}
+
+function iniciarRodadaOponente() {
+    alert('Vez do oponente!');
+    let rolagemOponente = rolarDadoOpo();
+    alert(`O oponente rolou no dado: ${rolagemOponente}`);
+    
+    let colunaOponente = Math.floor(Math.random() * 3) + 1;
+    escolherColuna(colunaOponente);
+
+    jogadorAtual = "jogador";
+    iniciarRodadaJogador();
+}
+
+function rolarDado() {
+    let resultadoDado = Math.floor(Math.random() * 6) + 1;
+    console.log(`Você rolou um ${resultadoDado}.`);
+    alert(`Você rolou um ${resultadoDado}. Escolha uma coluna para colocar o valor.`);
+
+    // Adiciona ouvintes de evento aos botões de coluna
+    document.getElementById('C1').addEventListener('click', function() {
+        escolherColuna(1);
+    });
+    document.getElementById('C2').addEventListener('click', function() {
+        escolherColuna(2);
+    });
+    document.getElementById('C3').addEventListener('click', function() {
+        escolherColuna(3);
+    });
+
+    // Remove ouvintes de evento dos botões de coluna
+    document.getElementById('C1').removeEventListener('click', iniciarRodadaOponente);
+    document.getElementById('C2').removeEventListener('click', iniciarRodadaOponente);
+    document.getElementById('C3').removeEventListener('click', iniciarRodadaOponente);
+}
+
+function escolherColuna(coluna) {
+    let colunaId = jogadorAtual === "jogador" ? `C${coluna}` : `CO${coluna}`;
+
+    let quadradosColuna = document.getElementById(colunaId).getElementsByClassName('quadrado');
+    let dadosColuna = [];
+    for (let i = 0; i < quadradosColuna.length; i++) {
+        if (quadradosColuna[i].innerText.trim()) {
+            dadosColuna.push(parseInt(quadradosColuna[i].innerText.trim()));
+        }
+    }
+
+    let pontos = calcularPontosColuna(dadosColuna, resultadoDado);
+
+    if (jogadorAtual === "jogador") {
+        pontosJogador += pontos;
+        atualizarPontos('pontosJogador', pontosJogador);
+
+        if (coluna === 1) pontosC1 = pontos;
+        else if (coluna === 2) pontosC2 = pontos;
+        else if (coluna === 3) pontosC3 = pontos;
+    } else {
+        pontosOponente += pontos;
+        atualizarPontos('pontosOponente', pontosOponente);
+
+        if (coluna === 1) pontosCO1 = pontos;
+        else if (coluna === 2) pontosCO2 = pontos;
+        else if (coluna === 3) pontosCO3 = pontos;
+    }
+
+    // Atualiza o texto da próxima div "quadrado" com o valor do dado
+    document.getElementById(`${colunaId}Q${dadosColuna.length + 1}`).innerText = resultadoDado;
+
+    // Remover ouvintes de evento dos botões de coluna
+    document.getElementById('C1').removeEventListener('click', function() {
+        escolherColuna(1);
+    });
+    document.getElementById('C2').removeEventListener('click', function() {
+        escolherColuna(2);
+    });
+    document.getElementById('C3').removeEventListener('click', function() {
+        escolherColuna(3);
+    });
+
+    // Alterna entre jogador e oponente
+    jogadorAtual = jogadorAtual === "jogador" ? "oponente" : "jogador";
+    
+    if (jogadorAtual === "oponente") {
+        iniciarRodadaOponente();
+    } else {
+        iniciarRodadaJogador();
+    }
+}
+
+function rolarDadoOpo() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+function calcularPontosColuna(coluna, dado) {
+    let ocorrencias = [0, 0, 0, 0, 0, 0];
+    for (let i = 0; i < coluna.length; i++) {
+        ocorrencias[coluna[i] - 1]++;
+    }
+
+    let pontos = 0;
+    for (let i = 0; i < ocorrencias.length; i++) {
+        pontos += ocorrencias[i] * (i + 1) * dado;
+    }
+
+    return pontos;
+}
+
+function atualizarPontos(idElemento, pontos) {
+    document.getElementById(idElemento).innerText = `Pontos: ${pontos}`;
+}
+
+// Iniciar a primeira rodada com o jogador
+iniciarRodadaJogador();
+
+
+/// Código com erros v.2
+
+/*let pontosC1 = 0;
+let pontosC2 = 0;
+let pontosC3 = 0;
+let pontosCO1 = 0;
+let pontosCO2 = 0;
+let pontosCO3 = 0;
+let pontosJogador = 0;
+let pontosOponente = 0;
+
+let jogadorAtual = "jogador";
+
 function rolarDado() {
     let resultadoDado = Math.floor(Math.random() * 6) + 1;
     console.log(`Você rolou um ${resultadoDado}.`);
@@ -66,19 +197,24 @@ function escolherColuna(coluna) {
         else if (coluna === 3) pontosCO3 = pontos;
     }
 
-    // Remover os ouvintes de evento após escolher a coluna
-    document.getElementById('C1').removeEventListener('click', function() {
-        escolherColuna(1);
+    // Adiciona ouvintes de evento aos botões de coluna fora da função rolarDado
+    document.getElementById('C1').addEventListener('click', function() {
+    escolherColuna(1);
     });
-    document.getElementById('C2').removeEventListener('click', function() {
-        escolherColuna(2);
+
+    document.getElementById('C2').addEventListener('click', function() {
+    escolherColuna(2);
     });
-    document.getElementById('C3').removeEventListener('click', function() {
-        escolherColuna(3);
+
+    document.getElementById('C3').addEventListener('click', function() {
+    escolherColuna(3);
     });
 
     // Atualiza as divs de pontos no HTML
     alert(`Pontos da coluna ${coluna}: ${pontos}`);
+
+    // Atualiza o texto da próxima div "quadrado" com o valor do dado
+    document.getElementById(`${colunaId}Q${dadosColuna.length + 1}`).innerText = resultadoDado;
 
     // Alterna entre jogador e oponente
     jogadorAtual = jogadorAtual === "jogador" ? "oponente" : "jogador";
@@ -112,7 +248,7 @@ function atualizarPontos(idElemento, pontos) {
     document.getElementById(idElemento).innerText = `Pontos: ${pontos}`;
 }
 
-document.getElementById('rodarDado').addEventListener('click', rolarDado);
+document.getElementById('rodarDado').addEventListener('click', rolarDado);/*
 
 
 /// Código com erro
