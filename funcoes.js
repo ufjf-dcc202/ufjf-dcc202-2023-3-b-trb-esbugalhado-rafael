786f463332c586495f1e7bdba4270cd3d091b451
Rafael Vicente Santrovitsch <rafael.santrovitsch@estudante.ufjf.br>
2023-12-11 23:39:55 -0300

    Criei mais algumas variaveis

--- funcoes.js
+++ funcoes.js
@@ -4,10 +4,20 @@ let colunasDoJogador = [
     [0, 0, 0]
     [0, 0, 0]
 ];
-
 let somaColunasJogador = [0, 0, 0];
-
 let somaTotalJogador = 0;
+let tabuleiroDoJogador;
+let somaTxtJogador;
+let pontuaçãoFileiraJogador;
+let somaTotalTxtJogador;
+
+// Funções do jogador
+
+function definirTabuleiroDoJogador(tabuleiro){
+    tabuleiroDoJogador = tabuleiro;
+}
+
+
 
 
 // Variaveis Oponente
@@ -16,10 +26,12 @@ let colunasDoOponente = [
     [0, 0, 0]
     [0, 0, 0]
 ];
-
 let somaColunasOponente = [0, 0, 0];
-
 let somaTotalOponente = 0;
+let tabuleiroDoOponente;
+let somaTxtOponente;
+let pontuaçãoFileiraOponente;
+let somaTotalTxtOponente;
 
 // Variaveis do Jogo
 
