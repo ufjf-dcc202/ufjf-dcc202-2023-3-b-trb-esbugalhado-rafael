27256aa119336483ea4f6b39fab002560f03c957
Rafael Vicente Santrovitsch <rafael.santrovitsch@estudante.ufjf.br>
2023-12-11 23:33:22 -0300

    Criei algumas variaveis

---
+++ funcoes.js
@@ -0,0 +1,28 @@
+// Variaveis jogador
+let colunasDoJogador = [
+    [0, 0, 0],
+    [0, 0, 0]
+    [0, 0, 0]
+];
+
+let somaColunasJogador = [0, 0, 0];
+
+let somaTotalJogador = 0;
+
+
+// Variaveis Oponente
+let colunasDoOponente = [
+    [0, 0, 0],
+    [0, 0, 0]
+    [0, 0, 0]
+];
+
+let somaColunasOponente = [0, 0, 0];
+
+let somaTotalOponente = 0;
+
+// Variaveis do Jogo
+
+let valorDoDado = 0;
+let valorDoDadoOpo = 0;
+let gameOver = false;
