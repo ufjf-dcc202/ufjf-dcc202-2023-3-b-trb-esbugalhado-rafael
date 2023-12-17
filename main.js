d7a83e8619eadd858f1d07c9e10aa4a853138d6d
Rafael Vicente Santrovitsch <rafael.santrovitsch@estudante.ufjf.br>
2023-12-17 12:48:12 -0300

    declarei variaveis e funções iniciais

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
+// - - - Variaveis
+
+let jogador;
+let oponente;
+let jogadorAtual;
+let valorDado;
+let valorPosição;
+let valorColuna;
+let valorTotalJogador;
+let valorTotalOponente;
+
 // - - - Funções
 
+function turnoJogador(){
+
+}
+
+function turnoOponente(){
+
+}
+
+function rodarDado(){
+    
+}
