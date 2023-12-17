de2976f18170e0ab13c53c0db7a756076fc3e021
Rafael Vicente Santrovitsch <rafael.santrovitsch@estudante.ufjf.br>
2023-12-17 13:00:45 -0300

    rolagem de dado para o oponente

--- main.js
+++ main.js
@@ -4,11 +4,14 @@ let jogador;
 let oponente;
 let jogadorAtual;
 let valorDado;
+let valorDadoOponente;
 let valorPosição;
 let valorColuna;
 let valorTotalJogador;
 let valorTotalOponente;
 let gameOver = false;
+
+// - - - Jogo
 
 
 // - - - Funções
@@ -18,8 +21,8 @@ function turnoJogador(){
 
 }
 
-function turnoOponente(){
-
+function turnoOponente(){ 
+    valorDadoOponente = Math.floor(Math.random()*6)+1;
 }
 
 function rodarDado(){
